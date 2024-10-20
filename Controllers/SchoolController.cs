9b1a5e00f47fc3df09458ff566a93992a20f5a25
unknown <hajbi662@gmail.com>
2024-10-20 21:19:28 +0100

    first commit.

---
+++ Controllers/SchoolController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using WebApplication3.Models.Repositories;
+
+namespace WebApplication3.Controllers
+{
+    
+    [Authorize(Roles = "Admin,Manager")]
+    public class SchoolController : Controller
+    {
+        private readonly ISchoolRepository _schoolRepository;
+
+        public SchoolController(ISchoolRepository schoolRepository)
+        {
+            _schoolRepository = schoolRepository;
+        }
+
+        // GET: SchoolController/Index
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            var schools = _schoolRepository.GetAll();
+            return View(schools);
+        }
+
+        // GET: SchoolController/Details/5
+        public IActionResult Details(int id)
+        {
+            var school = _schoolRepository.GetById(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+            return View(school);
+        }
+
+        // GET: SchoolController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SchoolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(School school)
+        {
+            try
+            {
+                _schoolRepository.Add(school);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            { 
+                return View(school);
+            }
+        }
+
+        // GET: SchoolController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var school = _schoolRepository.GetById(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+            return View(school);
+        }
+
+        // POST: SchoolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(School school)
+        {
+            if (ModelState.IsValid)
+            {
+                _schoolRepository.Edit(school);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(school);
+        }
+
+        // GET: SchoolController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var school = _schoolRepository.GetById(id);
+            if (school == null)
+            {
+                return NotFound();
+            }
+            return View(school);
+        }
+
+        // POST: SchoolController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(School school)
+        {
+            _schoolRepository.Delete(school);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using WebApplication3.Models.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize(Roles = "Admin,Manager")]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly ISchoolRepository schoolRepository;
+
+        public StudentController(IStudentRepository studentRepository, ISchoolRepository schoolRepository)
+        {
+            this.studentRepository = studentRepository;
+            this.schoolRepository = schoolRepository;
+        }
+
+
+        // GET: StudentController
+        [AllowAnonymous]
+        public IActionResult Index()
+{
+    var students = studentRepository.GetAll();  // Récupère tous les étudiants
+    if (students == null) 
+    {
+        return View(new List<Student>());  // Retourne une liste vide pour éviter un NullReferenceException
+    }
+
+    return View(students);
+}
+
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName");
+            return View();
+        } 
+
+
+        // Details
+        public ActionResult Details(int id)
+        {
+            var Student = studentRepository.GetById(id);
+
+            return View(Student);
+        }
+
+
+
+        // POST: StudentController/Create
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var student = studentRepository.GetById(id);
+
+                ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName", student.SchoolID);
+                return View(student);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    studentRepository.Edit(student);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName", student.SchoolID);
+                return View(student);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        public ActionResult Search(string name, int? schoolid)
+
+        {
+            var result = studentRepository.GetAll();
+            if (!string.IsNullOrEmpty(name))
+                result = studentRepository.FindByName(name);
+            else
+            if (schoolid != null)
+                result = studentRepository.GetStudentsBySchoolID(schoolid);
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName");
+
+            return View("Index", result);
+        }
+    }
+}
